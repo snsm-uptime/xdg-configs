118a59ba8d3e15b5476c99dd32fe2da023f196d6
Sebastian Soto M <s.m.sebastian.n@gmail.com>
2026-04-17 17:44:58 +0000

    fix: fzf bin install

--- fzf/install.sh
+++ fzf/install.sh
@@ -5,17 +5,24 @@ FZF_DIR="$XDG_DATA_HOME/fzf"
 FZF_THEME_SRC="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/tokyonight_moon.sh"
 FZF_THEME_DEST="$XDG_CONFIG_HOME/fzf/theme.sh"
 
-if [[ ! -x "$FZF_DIR/bin/fzf" ]]; then
+if [[ ! -d "$FZF_DIR" ]]; then
   log_info "Installing fzf → $FZF_DIR"
   if [[ "$DRY_RUN" == "false" ]]; then
-    [[ ! -d "$FZF_DIR" ]] && git clone --depth=1 https://github.com/junegunn/fzf.git "$FZF_DIR"
-    "$FZF_DIR/install"
+    git clone --depth=1 https://github.com/junegunn/fzf.git "$FZF_DIR"
+    "$FZF_DIR/install" --bin
     log_ok "fzf installed"
   else
     log_dry "git clone fzf $FZF_DIR && $FZF_DIR/install --bin"
   fi
 else
-  log_skip "fzf already installed"
+  log_info "Updating fzf → $FZF_DIR"
+  if [[ "$DRY_RUN" == "false" ]]; then
+    git -C "$FZF_DIR" pull --rebase
+    "$FZF_DIR/install" --bin
+    log_ok "fzf updated"
+  else
+    log_dry "git -C $FZF_DIR pull --rebase && $FZF_DIR/install --bin"
+  fi
 fi
 
 log_info "Linking fzf theme → $FZF_THEME_DEST"
